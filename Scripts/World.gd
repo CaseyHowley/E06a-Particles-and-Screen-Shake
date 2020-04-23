6794d26d517806beef647d253b16281f6e7f7bef
Daniel (Casey) Leland <59851941+CaseyHowley@users.noreply.github.com>
2020-04-23 10:02:57 -0400

    Completed Project

--- Scripts/World.gd
+++ Scripts/World.gd
@@ -6,4 +6,14 @@ func _ready():
 	pass
 
 func _physics_process(delta):
-	pass
+	if Input.is_key_pressed(87):
+		$Camera.add_trauma(0.1)
+
+	if Input.is_key_pressed(65):
+		$Camera.add_trauma(0.3)
+
+	if Input.is_key_pressed(83):
+		$Camera.add_trauma(0.5)
+
+	if Input.is_key_pressed(68):
+		$Camera.add_trauma(1)
